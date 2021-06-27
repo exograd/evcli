e2bbfb650a95b10591b72c29cc4decb85851985a
Nicolas Martyanoff <khaelin@gmail.com>
2021-06-27 17:32:41 +0200

    add the 'config show' command

--- cmd_config.go
+++ cmd_config.go
@@ -1,10 +1,35 @@
 package main
 
-import "github.com/galdor/go-cmdline"
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/galdor/go-cmdline"
+)
 
 func cmdConfig(args []string, client *Client) {
 	cl := cmdline.New()
+	cl.AddCommand("show", "print the configuration and exit")
 	cl.Parse(args)
 
-	// TODO
+	var cmd func([]string, *Client)
+
+	switch cl.CommandName() {
+	case "show":
+		cmd = cmdConfigShow
+	}
+
+	cmd(cl.CommandNameAndArguments(), client)
+}
+
+func cmdConfigShow(args []string, client *Client) {
+	cl := cmdline.New()
+	cl.Parse(args)
+
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "  ")
+
+	if err := encoder.Encode(client.Config); err != nil {
+		die("cannot encode configuration: %v", err)
+	}
 }
